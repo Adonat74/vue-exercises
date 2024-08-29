969da791b58f477379db06304e9b93de447e4530
Adonat74 <donatgoninet.antoine@gmail.com>
2024-08-29 14:55:09 +0200

    passages de props au composant city depuis citieslist

--- src/components/City.vue
+++ src/components/City.vue
@@ -1,19 +1,19 @@
 <script setup>
  
-let data = {
-    name: 'Ma ville', // nom de la ville
-    weather: 'Peu nuageux', // descriptif météo 
-    temperature: 20.55, // température en °C
-    updatedAt: new Date() // date de dernière mise à jour
-}
+ defineProps({
+    name: String, // nom de la ville
+    weather: String, // descriptif météo 
+    temperature: Number, // température en °C
+    updatedAt: Date // date de dernière mise à jour
+})
 
 </script>
 
 <template>
-  <h1>{{data.name}}</h1>
-  <p>Temps : {{ data.weather }}</p>
-  <p>Température : {{ data.temperature }} C°</p>
-  <p>mise à jour : {{ data.updatedAt.toLocaleString() }}</p>
+  <h1>{{name}}</h1>
+  <p>Temps : {{ weather }}</p>
+  <p>Température : {{ temperature }} C°</p>
+  <p>mise à jour : {{ updatedAt.toLocaleString() }}</p>
 </template>
 
 <style scoped>
--- src/views/CitiesList.vue
+++ src/views/CitiesList.vue
@@ -1,14 +1,18 @@
 <script setup>
- 
+import City from "../components/City.vue";
 
+let data = {
+  name: "Ma ville", // nom de la ville
+  weather: "Peu nuageux", // descriptif météo
+  temperature: 20.55, // température en °C
+  updatedAt: new Date(), // date de dernière mise à jour
+};
 </script>
 
 <template>
   <h1>Liste ville Météo</h1>
   <RouterLink to="/ville">ville</RouterLink>
-
+  <City v-bind="data" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
