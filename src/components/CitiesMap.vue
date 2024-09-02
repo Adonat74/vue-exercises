9a8ff1ef5be123cd241348c3da14d652c91c5863
Adonat74 <donatgoninet.antoine@gmail.com>
2024-09-02 10:04:59 +0200

    imported leaflet and made a simple map component

---
+++ src/components/CitiesMap.vue
@@ -0,0 +1,34 @@
+<script setup>
+import "leaflet/dist/leaflet.css"
+import { ref } from "vue";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet"
+
+// import arcades from "./arcades.json"
+
+let zoom = ref(6)
+let center = ref([38, 139.69])
+</script>
+
+<template>
+    <main style="height:600px; width:800px">
+        <l-map ref="map" v-model:zoom="zoom" v-model:center="center" :useGlobalLeaflet="false">
+            <l-tile-layer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                    layer-type="base"
+                    name="Stadia Maps Basemap">
+            </l-tile-layer>
+        </l-map>
+    </main>
+</template>
+
+
+<style>
+    html, body {
+        margin: 0;
+        padding: 0;
+    }
+
+    main {
+        height: 100vh;
+        width: 100vw;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
 import CitiesList from '../views/CitiesList.vue';
-import City from '../components/City.vue';
+//import City from '../components/City.vue';
+import CitiesMap from '../components/CitiesMap.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes:[
         {path: '/', name:"home" ,component: HelloWorld},
         {path: '/villes', name:"villes" , component: CitiesList},
-        //{path: '/ville', name:"ville" , component: City},
+        {path: '/carte', name:"carte" , component: CitiesMap},
     ]
 });
 
